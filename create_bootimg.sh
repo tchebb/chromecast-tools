9d82645d26154bb9b06ccd15af34e697d0416546
Thomas Hebb <tommyhebb@gmail.com>
2013-08-21 19:49:21 -0400

    Add boot image creation script

---
+++ create_bootimg.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+
+if test "$#" -ne 3 ; then
+	echo "Usage: $0 KERNEL INITRAMFS OUTFILE"
+	exit 1
+fi
+
+INITRAMFS="$(mktemp)"
+pushd "$2" >/dev/null
+find . | cpio -o -H newc 2>/dev/null | gzip > "$INITRAMFS"
+popd >/dev/null
+
+BOOTIMG="$(mktemp)"
+mkbootimg --kernel "$1" --ramdisk "$INITRAMFS" -o "$BOOTIMG"
+
+rm "$INITRAMFS"
+
+./mangle_bootimg "$BOOTIMG" "$3"
+
+rm "$BOOTIMG"
